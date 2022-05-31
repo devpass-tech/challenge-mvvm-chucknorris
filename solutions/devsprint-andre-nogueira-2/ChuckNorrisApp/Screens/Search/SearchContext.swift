eb84ce29a1af41533b993998768bab3e29aa073d
Bruno <brunobraganca@gmail.com>
2022-05-31 18:28:43 -0300

    Added missing files

---
+++ solutions/devsprint-andre-nogueira-2/ChuckNorrisApp/Screens/Search/SearchContext.swift
@@ -0,0 +1,13 @@
+//
+//  SearchContext.swift
+//  ChuckNorrisApp
+//
+//  Created by Appito on 30/05/22.
+//
+
+import Foundation
+
+class SearchContext: ObservableObject {
+    @Published var searchText = ""
+}
+
